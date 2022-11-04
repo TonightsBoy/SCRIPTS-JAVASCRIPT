9f08b899e9e4067a148218a875bd6a3794ed6e4b
TonightsBoy <109231542+TonightsBoy@users.noreply.github.com>
2022-11-04 19:41:25 +0000

    Delete placeholder

--- cardGame.js
+++ cardGame.js
@@ -2,3 +2,5 @@
 var cardHTML = document.getElementById("card");
 var scoreHTML = document.getElementById("score");
 var resultHTML = document.getElementById("result");
+
+var possibleCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
