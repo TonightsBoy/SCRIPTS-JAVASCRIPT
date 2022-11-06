a1f32a996f60bd049291f32a893100b174562555
TonightsBoy <109231542+TonightsBoy@users.noreply.github.com>
2022-11-06 23:04:20 +0000

    Fixed travis configuration

--- cardGame.js
+++ cardGame.js
@@ -6,3 +6,6 @@ var resultHTML = document.getElementById("result");
 var possibleCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 var previousCard = 0;
 var randomNumber = 0;
+
+var numQuestionsAsked = 0;
+var numCorrect = 0;
